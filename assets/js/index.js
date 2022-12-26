aaf25ff422af3cad46e7e898bf87bfe8420005f1
IlkinBairamov <ilkinabr@code.edu.az>
2022-12-26 17:43:01 +0400

    progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,35 +16,50 @@
 //     })
 // // })
 
-$(document).ready(function(){
+//$(document).ready(function(){
     // $(".navbar-toggler").click(function(){
     //     $(".navList").slideDown("slow")
     //     $(".navList").css({"display":"block","right":"20px","left":"260px"});
     //     $(".dropdown-menu").css({"left":"1%","top":"28px"})
     //   });
 
-      $(window).scroll(function () {
-        let height=$(window).scrollTop()
-        if( $("#navbar").hasClass("transparant")){
-        if (height>0) {
-            $("#navbar").css({"background-color":"white","position":"fixed","top":"0"})
-        }
-        else
-        {
-            $("#navbar").css({ "position": "absolute",
-            "background-color": "transparent"})
-        }
-    }})
+//       $(window).scroll(function () {
+//         let height=$(window).scrollTop()
+//         if( $("#navbar").hasClass("transparant")){
+//         if (height>0) {
+//             $("#navbar").css({"background-color":"white","position":"fixed","top":"0"})
+//         }
+//         else
+//         {
+//             $("#navbar").css({ "position": "absolute",
+//             "background-color": "transparent"})
+//         }
+//     }})
 
-    $(window).scroll(function () {
-        let height=$(window).scrollTop()
-        if (height>500) {
-            $(".scroll-icon").addClass("scroll-active")
-        }
-        else{
-            $(".scroll-icon").removeClass("scroll-active")
-        }
-    })
-})
+//     $(window).scroll(function () {
+//         let height=$(window).scrollTop()
+//         if (height>500) {
+//             $(".scroll-icon").addClass("scroll-active")
+//         }
+//         else{
+//             $(".scroll-icon").removeClass("scroll-active")
+//         }
+//     })
+// })
 
     
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
