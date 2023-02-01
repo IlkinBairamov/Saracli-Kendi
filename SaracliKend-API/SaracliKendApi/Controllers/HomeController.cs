252577481ba35cb4c818288881bfdfefc594fced
Aytaj Ramazanli <AytajRamazanli@risk.az>
2023-02-01 13:25:12 +0400

    used information for banner content

--- SaracliKend-API/SaracliKendApi/Controllers/HomeController.cs
+++ SaracliKend-API/SaracliKendApi/Controllers/HomeController.cs
@@ -12,14 +12,16 @@ namespace SaracliKendApi.Controllers
         private readonly IPersonService _personService;
         private readonly INewsService _newsService;
         private readonly IFileService _fileService;
+        private readonly IInformationService _informationService;
 
-        public HomeController(ILogger<HomeController> logger, ISliderImageService sliderImageService, IPersonService personService, INewsService newsService, IFileService fileService)
+        public HomeController(ILogger<HomeController> logger, ISliderImageService sliderImageService, IPersonService personService, INewsService newsService, IFileService fileService, IInformationService informationService)
         {
             _logger = logger;
             _sliderImageService = sliderImageService;
             _personService = personService;
             _newsService = newsService;
             _fileService = fileService;
+            _informationService = informationService;
         }
 
         public async Task<IActionResult> Index()
@@ -30,6 +32,7 @@ namespace SaracliKendApi.Controllers
             var news = await _newsService.GetAllAsync();
             var photos = await _fileService.GetFilesByType(FileType.Image);
             var videos = await _fileService.GetFilesByType(FileType.Video);
+            var info = await _informationService.GetInformation(InformationType.General);
 
             var homeVM = new HomeViewModel
             {
@@ -38,7 +41,8 @@ namespace SaracliKendApi.Controllers
                 Ghazis = ghazis.Count > 0 ? ghazis.GetRange(0, ghazis.Count < 3 ? ghazis.Count : 3) : ghazis,
                 News = news,
                 Photos = photos.Count > 0 ? photos.GetRange(0, photos.Count < 3 ? photos.Count : 3) : photos,
-                Videos = videos.Count > 0 ? videos.GetRange(0, videos.Count < 3 ? videos.Count : 3) : videos
+                Videos = videos.Count > 0 ? videos.GetRange(0, videos.Count < 3 ? videos.Count : 3) : videos,
+                Information = info
             };
             return View(homeVM);
         }
