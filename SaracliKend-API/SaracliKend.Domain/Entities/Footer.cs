5842f78dc7dc14ae94df41f19371c15a053d7790
Aytaj Ramazanli <AytajRamazanli@risk.az>
2023-02-01 13:18:21 +0400

    create footer Entity

---
+++ SaracliKend-API/SaracliKend.Domain/Entities/Footer.cs
@@ -0,0 +1,19 @@
+﻿using SaracliKend.Domain.Common.Contracts;
+
+namespace SaracliKend.Domain.Entities
+{
+    public class Footer : IEntity
+    {
+        public int Id { get; set; }
+
+        public string About { get; set; } = string.Empty;
+
+        public string PhoneNumebr { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string FacebookUrl { get; set; } = string.Empty;
+
+        public string InstagramUrl { get; set; } = string.Empty;
+    }
+}
