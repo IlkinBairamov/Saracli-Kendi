f17a5d0f53c421582fdfcc8a755afef6b4dba58e
Aytaj Ramazanli <AytajRamazanli@risk.az>
2023-02-01 13:22:06 +0400

    added footer view component and invoke in layout

---
+++ SaracliKend-API/SaracliKendApi/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaracliKend.Application.Services.Contracts;
+
+namespace SaracliKendApi.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly IFooterService _footerService;
+
+        public FooterViewComponent(IFooterService footerService)
+        {
+            _footerService = footerService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var footerInfo = await _footerService.GetFooterAsync();
+            return View(footerInfo);
+        }
+    }
+}
